3a59117979ac3334d18fda8c41f5396e31d2cac8
Xio26 <inesxiomaravasquezaguirre@gmail.com>
2023-06-16 11:08:17 -0500

    Actualizacion de Modulo 1

--- addons/modulo_1/__manifest__.py
+++ addons/modulo_1/__manifest__.py
@@ -2,7 +2,7 @@
     "name":"modulo_1",
     "version":"1.0",
     "summary":"Modulo 1",
-    "description":"Primer Modulo de Prueba",
+    "description":"Primer Modulo: Lista de Tareas",
     "author":"Xiomara Vásquez",
     "company":"XV",
     "category":"Management System",
